9960289ac7a71f7ceb9aa28de8db462289f6dc0b
cxrvus <benito.m.rabe@gmail.com>
2025-03-08 23:09:21 +0100

    extracted settings class from main.ts to settings.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { App, Command, Notice, Plugin, PluginSettingTab, Setting, SuggestModal } from 'obsidian';
+import { App, Command, Notice, Plugin, SuggestModal } from 'obsidian';
 import * as archiver from './archiver'
-import { getPathsFromFolderList } from './util';
+import { ArchiverSettingsTab } from './settings';
 
 
 interface ArchiverSettings {
@@ -87,13 +87,6 @@ export default class Archiver extends Plugin {
 		ribbonButton.setAttribute("aria-label", "Advanced Archiver");
 
 		this.addSettingTab(new ArchiverSettingsTab(this.app, this));
-
-		// ## Settings:
-		// fixme: move all validation to saveSettings()
-	}
-
-	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -105,94 +98,3 @@ export default class Archiver extends Plugin {
 	}
 }
 
-
-class ArchiverSettingsTab extends PluginSettingTab {
-	plugin: Archiver;
-
-	constructor(app: App, plugin: Archiver) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		// TODO: allow paths to be empty and handle those cases
-
-		new Setting(containerEl)
-			.setName('Archive Folder')
-			.setDesc('target folder for you archived notes')
-			.addText(text => text
-				.setPlaceholder('Folder')
-				.setValue(this.plugin.settings.targetFolder)
-				.onChange(async (value) => {
-					if (!value) new Notice("please specify a folder!");
-					else if (!this.app.vault.getFolderByPath(value)) new Notice("please specify a valid folder!");
-					else {
-						this.plugin.settings.targetFolder = value;
-						await this.plugin.saveSettings();
-					}
-				})
-			)
-		;
-
-		new Setting(containerEl)
-			.setName('Search Folders')
-			.setDesc('folders that will be searched by Archiver, separated by commas')
-			.addText(text => text
-				.setPlaceholder('Folders')
-				.setValue(this.plugin.settings.includedFolders)
-				.onChange(async (value) => {
-					try {
-						getPathsFromFolderList(this.plugin, value);
-					}
-					catch (e) {
-						return;
-					}
-
-					new Notice('successfully changed included folders');
-
-					this.plugin.settings.includedFolders = value;
-					await this.plugin.saveSettings();
-				})
-			)
-		;
-
-		new Setting(containerEl)
-			.setName('Root File')
-			.setDesc('file that will be used to recursively detect untracked files')
-			.addText(text => text
-				.setPlaceholder('File')
-				.setValue(this.plugin.settings.rootFile)
-				.onChange(async (value) => {
-					if (!this.app.vault.getFileByPath(value)) return
-
-					new Notice('successfully changed root file');
-
-					this.plugin.settings.rootFile = value;
-					await this.plugin.saveSettings();
-				})
-			)
-		;
-
-		const CanvasMirrorDescription = document.createDocumentFragment();
-		const CanvasMirror = document.createElement('a');
-		CanvasMirror.href = 'https://github.com/cxrvus/canvas-mirror';
-		CanvasMirror.textContent = 'Canvas Mirror';
-		CanvasMirrorDescription.append('exclude ', CanvasMirror, ' files from being archived');
-
-		new Setting(containerEl)
-			.setName('Exclude Canvas Mirrors')
-			.setDesc(CanvasMirrorDescription)
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.excludeMirrors)
-				.onChange(async (value) => {
-					this.plugin.settings.excludeMirrors = value;
-					await this.plugin.saveSettings();
-				})
-			)
-		;
-	}
-}
